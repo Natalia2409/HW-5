fcef3fabbb94100b04c57ddc0de0d31973a06fbe
Natalia2409 <65865345+Natalia2409@users.noreply.github.com>
2020-08-01 21:49:03 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,205 +2,194 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 	// Function 1
-
-	let func1Btn = document.getElementById('func1_btn');
-	func1Btn.addEventListener('click', () => {
-		let result = document.getElementById('func1_result');
-		let func1Length = document.getElementById('length').value;
-		let func1Min = document.getElementById('min').value;
-		let func1Max = document.getElementById('max').value;
-		func1Length = Number(func1Length);
-		func1Min = Number(func1Min);
-		func1Max = Number(func1Max);
-		let newArray = [];
-		if (func1Min > func1Max)  {
-			newArray.push('Друге число менше, ніж перше!');
-		} else if (func1Length === '' || func1Min === '' || func1Max === '') {
-			newArray.push('Ви ввели неправильне значення!');
+	function getRandomArray(length, min, max) {
+		let randomArray = [];
+		if (max < min) {
+			throw 'ВИ ввели неправильні дані';
 		} else {
-			for (let i = 0; i < func1Length; i++) {
-				const randomNum = Math.floor(Math.random() * (func1Max - func1Min + 1) + func1Min);
-				newArray.push(randomNum);
+			for (let i = 0; i < length; i++) {
+				randomArray[i] = Math.floor(Math.random() * (max - min + 1) + min);
 			}
+			return randomArray;
 		}
-		result.innerHTML = `<p>${newArray}</p>`
-	});
-
+	}
 
 	// Function 2
-
-	func2_btn.addEventListener('click', () => {
-		let func2Btn = document.getElementById('func2_btn');
-		let result2 = document.getElementById('func2_result');
-		let moda = document.getElementById('length1').value.split(',');
-		let newMass = [];
-		for (let i = 0; i < moda.length; i++) {
-			if (Number(moda[i])) {
-				newMass.push(moda[i]);
-			}
-		}
-		let arr1 = [];
-		for (let i = 0; i < newMass.length; i++) {
-			arr1.push(parseInt(newMass[i]));
-		}
-
-		function getMode(arr1) {
-    	let key, result = [ ],
+	function getMode(...numbers) {
+		let key, result = [ ],
 	        count = { }, max = -Infinity,
 	        nomodeMsg = "Масив не має моди.",
 	        nomodeFlag = true;
 
-	    arr1.forEach( (item) => {
+	    numbers.forEach((item) => {
 	        if (!(item in count)) count[item] = 0;
 	        count[item]++;
 	        max = max < count[item] ? count[item] : max;
 	    });
 	    if (max <= 1) return nomodeMsg;
 	    for (key in count) {
-	        if ( count[key] === max ) {
+	        if (count[key] === max ) {
 	            result.push(key)
 	        } else {
 	            nomodeFlag = false;
 	        }
 	    }
 	    return nomodeFlag ? nomodeMsg : result;
-		} 
+	} 
 
-	
-		result2.innerHTML = `<p>Мода масиву: ${getMode(arr1)}</p>`;
-	});
-
-	//Function 3
-
-	func3_btn.addEventListener('click', () => {
-		let func3Btn = document.getElementById('func3_btn');
-		let result3 = document.getElementById('func3_result');
-		let arrAverage = [];
-		let average = document.getElementById('length2').value.split(',');
+	// Function 3
+	function getAverage(...numbers) {
+		let averageArr = numbers.filter(num => Number(num));
 		let count = 0;
-		let last = [];
-		for (let i = 0; i < average.length; i++) {
-			last.push(parseInt(average[i]));
+		for (let i = 0; i < averageArr.length; i++) {
+			count += averageArr[i];
 		}
-		for (let j = 0; j < last.length; j++) {
-			count += last[j]
-		}
-		console.log(count);
-		let result = count / last.length;
-		result3.innerHTML = `<p>Середнє арифметичне масиву: ${result}</p>`;
-	});
+		let result = count / averageArr.length;
+		return result;
+	}
 
-	//Function 4
-
-	func4_btn.addEventListener('click', () => {
-		let func4Btn = document.getElementById('func4_btn');
-		let result4 = document.getElementById('func4_result');
-		let mediana = document.getElementById('length3').value.split(',');
-		let newMed = [];
-		for (let i = 0; i < mediana.length; i++) {
-			if (Number(mediana[i])) {
-				newMed.push(mediana[i]);
-			}
-		}
-		let medLast = [];
-		for (let i = 0; i < newMed.length; i++) {
-			medLast.push(parseInt(newMed[i]));
-		}
-		medLast = medLast.sort(function(a,b) {
+	// Function 4
+	function getMedian(...numbers) {
+		let medianaArr = numbers.filter(num => Number(num));
+		medianaArr = medianaArr.sort(function(a,b) {
 			return a - b;
 		});
 		let count = 0;
-		if (medLast.length % 2 === 0) {
-			count = (medLast[(medLast.length / 2) - 1] + medLast[medLast.length / 2]) / 2;
+		if (medianaArr.length % 2 === 0) {
+			count = (medianaArr[(medianaArr.length / 2) - 1] + medianaArr[medianaArr.length / 2]) / 2;
 		} else {
-			count = (medLast[Math.floor(medLast.length / 2)]);
+			count = (medianaArr[Math.floor(medianaArr.length / 2)]);
 		}
-		result4.innerHTML = `<p>Медіана масиву: ${count}</p>`;
-	});
+		return count;
+	}
 
-	//Function 5
-
-	func5_btn.addEventListener('click', () => {
-		let func5Btn = document.getElementById('func5_btn');
-		let result5 = document.getElementById('func5_result');
-		let evenNumbers = document.getElementById('length4').value.split(',');
-		let newEven = [];
-		for (let i = 0; i < evenNumbers.length; i++) {
-			if (Number(evenNumbers[i])) {
-				newEven.push(evenNumbers[i]);
-			}
-		}
-		let even = [];
-		for (let k = 0; k < newEven.length; k++) {
-			even.push(parseInt(newEven[k]));
-		}
-		const result = even.filter(evenNum => evenNum % 2 !== 0);
-		result5.innerHTML = `<p>Масив без парних чисел: ${result}</p>`;
-	});
+	// Function 5
+	function filterEvenNumbers(...numbers) {
+		let evenNum = numbers.filter(num => Number(num));
+		return evenNum.filter(evenNum => evenNum % 2 !== 0);
+	}
 
 	// Function 6
-
-	func6_btn.addEventListener('click', () => {
-		let func6Btn = document.getElementById('func6_btn');
-		let result6 = document.getElementById('func6_result');
-		let moreThanZero = document.getElementById('length5').value.split(',');
-		let newZero = [];
-		for (let i = 0; i < moreThanZero.length; i++) {
-			if (Number(moreThanZero[i])) {
-				newZero.push(moreThanZero[i]);
-			}
-		}
-		const noZero = [];
-		for (let i = 0; i < newZero.length; i++) {
-			if (newZero[i] > 0) {
-				noZero.push(newZero[i]);
-			}
-		}
-		result6.innerHTML = `<p>Кількість чисел, більших за 0: ${noZero.length}</p>`;
-	});
+	function countPositiveNumbers(...numbers) {
+		let countPos = numbers.filter(num => Number(num));
+		return countPos.filter(positive => positive > 0);
+	}
 
 	// Function 7
+	function getDividedByFive(...numbers) {
+		let divide = numbers.filter(num => Number(num));
+		return divide.filter(five => five % 5 === 0);
+	}
 
-	func7_btn.addEventListener('click', () => {
-		let func7Btn = document.getElementById('func7_btn');
-		let result7 = document.getElementById('func7_result');
-		let divisionFive = document.getElementById('length6').value.split(',');
-		let newFive = [];
-		for (let i = 0; i < divisionFive.length; i++) {
-			if (Number(divisionFive[i])) {
-				newFive.push(divisionFive[i]);
-			}
-		}
-		const onlyFive = [];
-		for (let i = 0; i < newFive.length; i++) {
-			if (newFive[i] % 5 === 0) {
-				onlyFive.push(newFive[i]);
-			}
-		}
-		result7.innerHTML = `<p>Числа, які ділять на 5 без остачі: ${onlyFive}</p>`;
-	});
-
-	//Function 8
-
-	func8_btn.addEventListener('click', () => {
-		let func8Btn = document.getElementById('func8_btn');
-		let result8 = document.getElementById('func8_result');
-		let changeWord = document.getElementById('length7').value.split(' ');
+	// Function 8
+	function replaceBadWords(string) {
 		const badWords = ['fuck', 'shit'];
-		let re = new RegExp(badWords.join('|'), 'gi');
-		let withoutBadWords = changeWord.map(element => element.replace(re, '****')).join(' ');
-		result8.innerHTML = `<p>Фраза без поганих слів: ${withoutBadWords}</p>`;
+		let replaceWord = new RegExp(badWords.join('|'), 'gi');
+		let withoutBadWords = string.map(element => element.replace(replaceWord, '****')).join(' ');
+		return withoutBadWords;
+	}
+
+	// Function 9
+	function divideByThree(word) {
+		return word.match(new RegExp('.{1,3}', 'g'));
+	}
+
+
+	let btn1 = document.getElementById('func1_btn');
+	let result1 = document.getElementById('func1_result');
+
+	btn1.addEventListener('click', () => {
+		let length1 = Number(document.getElementById('length').value);
+		let min1 = Number(document.getElementById('min').value);
+		let max1 = Number(document.getElementById('max').value);
+		let array = getRandomArray(length1, min1, max1);
+		result1.innerHTML = `Масив випадкових чисел: ${array}`
+	}); 
+
+
+	let btn2 = document.getElementById('func2_btn');
+	let result2 = document.getElementById('func2_result');
+
+	btn2.addEventListener('click', () => {
+		let moda = document.getElementById('length1').value.split(',');
+		let modaArray = moda.map(num => Number(num));
+		let result = getMode(...modaArray);
+		result2.innerHTML = `Мода масиву: ${result}`
 	});
 
-	//Function 9
 
-	func9_btn.addEventListener('click', () => {
-		let func9Btn = document.getElementById('func9_btn');
-		let result9 = document.getElementById('func9_result');
-		let splitWord = document.getElementById('length8').value;
-		console.log(splitWord);
-		let newWord = splitWord.match(new RegExp('.{1,3}', 'g'));
-		result9.innerHTML = `<p>Поділене слово: ${newWord}</p>`;
+	let btn3 = document.getElementById('func3_btn');
+	let result3 = document.getElementById('func3_result');
+
+	btn3.addEventListener('click', () => {
+		let average = document.getElementById('length2').value.split(',');
+		let averageNum = average.map(num => Number(num));
+		let result = getAverage(...averageNum);
+		result3.innerHTML = `Середнє арифметичне масиву: ${result}`
+	});
+
+
+	let btn4 = document.getElementById('func4_btn');
+	let result4 = document.getElementById('func4_result');
+
+	btn4.addEventListener('click', () => {
+		let mediana = document.getElementById('length3').value.split(',');
+		let medianaArray = mediana.map(med => Number(med));
+		let result = getMedian(...medianaArray);
+		result4.innerHTML = `Медіана масиву: ${result}`
+	});
+
+
+	let btn5 = document.getElementById('func5_btn');
+	let result5 = document.getElementById('func5_result');
+
+	btn5.addEventListener('click', () => {
+		let evenNumbers = document.getElementById('length4').value.split(',');
+		let evenArr = evenNumbers.map(num => Number(num));
+		let result = filterEvenNumbers(...evenArr);
+		result5.innerHTML = `Масив без парних чисел: ${result}`
+	});
+
+
+	let btn6 = document.getElementById('func6_btn');
+	let result6 = document.getElementById('func6_result');
+
+	btn6.addEventListener('click', () => {
+		let moreThanZero = document.getElementById('length5').value.split(',');
+		let positiveNum = moreThanZero.map(pos => Number(pos));
+		let result = countPositiveNumbers(...positiveNum);
+		result6.innerHTML = `Кількість додатніх чисел: ${result.length}`
+	});
+
+
+	let btn7 = document.getElementById('func7_btn');
+	let result7 = document.getElementById('func7_result');
+
+	btn7.addEventListener('click', () => {
+		let divisionFive = document.getElementById('length6').value.split(',');
+		let fiveArray = divisionFive.map(division => Number(division));
+		let result = getDividedByFive(...fiveArray);
+		result7.innerHTML = `Числа, які діляться на 5: ${result}`
+	});
+
+
+	let btn8 = document.getElementById('func8_btn');
+	let result8 = document.getElementById('func8_result');
+
+	btn8.addEventListener('click', () => {
+		let changeWord = document.getElementById('length7').value.split(' ');
+		let result = replaceBadWords(changeWord);
+		result8.innerHTML = `Без поганих слів: ${result}`
+	});
+
+
+	let btn9 = document.getElementById('func9_btn');
+	let result9 = document.getElementById('func9_result');
+
+	btn9.addEventListener('click', () => {
+		let word = document.getElementById('length8').value;
+		let result = divideByThree(word);
+		result9.innerHTML = `Розділене слово: ${result}`
 	});
 });
 
